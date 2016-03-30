da1dfdf69d1a78890316e670c080dff8ada57b51
corentinPaul <corentin.d.p.paul44@gmail.com>
2016-03-30 16:23:49 +0200

    sql script v2

--- src/base.sql
+++ src/base.sql
@@ -1,24 +1,27 @@
 CREATE TABLE Installation{
-numero int
-nom varchar(100),
-adresse varchar (100),
-code_postal varchar (6),
-ville varchar (50),
-latitude decimal,
-longitude decimal,
-PRIMARY KEY (numero)
+numero_inst int, // numéro d Installation
+nom_usel_inst varchar(100),// nom usuel
+nom_de_voie varchar (100), //addres de l Installation
+code_postal varchar (6), // code postal de la ville
+commune varchar (50), // commune qui hebere l installation
+lieu-dit varchar(50), // nom du lieu dit
+PRIMARY KEY (numero_inst)
 }
 
 CREATE TABLE Equipement{
-numero int,
-nom varchar(50),
-PRIMARY KEY (numero,nom),
-FOREIGN KEY (numero) REFERENCES Installation(numero)
+equipement_id int //numéro équipement,
+nom_equip varchar(50), //nom de l équipement
+numero_inst int, //numéro d installation
+longitude decimal,
+latitude decimal,
+PRIMARY KEY (equipement_id,nom_equip),
+FOREIGN KEY (numero_inst) REFERENCES Installation(numero_inst)
 }
 
 CREATE TABLE Activite{
-numero int,
-nom varchar(50),
-PRIMARY KEY (numero,nom),
-FOREIGN KEY (numero) REFERENCES Installation(numero)
+act_code int,// numero d Activite
+libv_acti varchar(100), // nom de l Activite
+equipement_id varchar(50), // numero d Equipement
+PRIMARY KEY (equipement_id,act_code),
+FOREIGN KEY (equipement_id) REFERENCES Installation(equipement_id)
 }
