7f3b8d5887c2dcececbd837009a602c26b24c49d
florian-detourne <florian.d44@hotmail.fr>
2016-03-30 16:15:25 +0200

    commit

--- serveur/server.py
+++ serveur/server.py
@@ -1,7 +1,8 @@
 from libs.bottle import route, template, run
 
+
 @route('/hello/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('template', name=name)
 
 run(host='localhost', port=8080)
